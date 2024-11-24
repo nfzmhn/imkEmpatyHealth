1e787a4df5b86e58b52ec7cbce414695fcaa646a
RetnoNS <119503025+RetnoNS@users.noreply.github.com>
2024-11-24 20:50:31 +0700

    update layanan

--- public/halamanutama/halamanutama.js
+++ public/halamanutama/halamanutama.js
@@ -7,3 +7,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const buatJanji = document.getElementById("buat-janji");
+const rekamMedis = document.getElementById("rekam-medis");
+const konsultasiOnline = document.getElementById("konsultasi-online");
+const jadwalDokter = document.getElementById("jadwal-dokter");
+
+buatJanji.addEventListener("click", function () {
+    window.location.href = "../buatjanji/buatjanji.html"; // Ganti dengan path file HTML tujuan
+});
+
+rekamMedis.addEventListener("click", function () {
+    window.location.href = "../rekammedis/rekammedis.html"; // Ganti dengan path file HTML tujuan
+});
+
+konsultasiOnline.addEventListener("click", function () {
+    window.location.href = "../komunitasonline/komunitasonline.html"; // Ganti dengan path file HTML tujuan
+});
+
+jadwalDokter.addEventListener("click", function () {
+    window.location.href = "../jadwaldok/jadwaldok.html"; // Ganti dengan path file HTML tujuan
+});
