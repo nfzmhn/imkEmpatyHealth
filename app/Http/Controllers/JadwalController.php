b152027496b712e3730055237ad311ef6e9b837c
Refa Muhammad <refamuhammadr@google.com>
2024-12-10 23:36:10 +0700

    Update riwayat user

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -10,7 +10,9 @@ class JadwalController extends Controller
     public function index()
     {
         // Ambil data dokter spesialis beserta jadwalnya
-        $dokterSpesialis = Dokter::whereHas('spesialis')->with('jadwals')->get();
+        $dokterSpesialis = Dokter::whereHas('spesialis', function ($query) {
+            $query->where('id_spesialis', '!=', 31);
+        })->with('jadwals')->get();
 
         // Ambil data dokter umum (id_spesialis = 31) beserta jadwalnya
         $dokterUmum = Dokter::where('id_spesialis', 31)->with('jadwals')->get();
